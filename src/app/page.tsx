b49a2845a7508dd5c7a0595290eb48b707b31527
avikantz <avikantsainidbz@gmail.com>
2023-08-17 17:43:43 +0530

    Setup chakra ui Adds base jumble game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,144 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Container,
+  HStack,
+  Heading,
+  Image,
+  PinInput,
+  PinInputField,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { IKEAProduct } from "@/interfaces";
 
 export default function Home() {
+  const [word, setWord] = useState<string>();
+  const [shuffledWord, setShuffledWord] = useState<string>();
+  const [product, setProduct] = useState<IKEAProduct>();
+
+  const [guess, setGuess] = useState<string>();
+  const [attempts, setAttempts] = useState<number>(0);
+
+  const getWords = useCallback(async () => {
+    try {
+      const response = await fetch("/api/jumble");
+      const res: { word: string; data: IKEAProduct } = await response.json();
+      setWord(res.word);
+      setShuffledWord(
+        res.word
+          .split("")
+          .sort(() => Math.random() - 0.5)
+          .join("")
+      );
+      setProduct(res.data);
+    } catch (error) {
+      // ???
+      setWord(undefined);
+      setShuffledWord(undefined);
+      setProduct(undefined);
+    } finally {
+      setGuess("");
+      setAttempts(0);
+    }
+  }, []);
+
+  useEffect(() => {
+    getWords();
+  }, [getWords]);
+
+  const onGuessChange = (value: string) => {
+    setGuess(value.toUpperCase());
+    if (word && value.length === word.length) {
+      setAttempts(attempts + 1);
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
+      <Box h="100vh">
+        <Container>
+          <VStack py="24" spacing="8">
+            <Heading textAlign="center">IKEA Jumble</Heading>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <HStack spacing="8" px="6" py="2" rounded="md" bg="gray.50">
+              {shuffledWord?.split("").map((w, i) => (
+                <Text key={`word-${w}${i}`} fontSize="2xl" fontWeight="light">
+                  {w}
+                </Text>
+              ))}
+            </HStack>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+            {word && (
+              <HStack>
+                <PinInput
+                  size="lg"
+                  autoFocus
+                  value={guess}
+                  onChange={onGuessChange}
+                  type="alphanumeric"
+                  isDisabled={guess === word || attempts >= 3}
+                >
+                  {Array(word.length)
+                    .fill(0)
+                    .map((_, i) => (
+                      <PinInputField textTransform="uppercase" key={i} />
+                    ))}
+                </PinInput>
+              </HStack>
+            )}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+            {guess === word && (
+              <Text textAlign="center" fontSize="xl" color="green.500">
+                You got it!
+              </Text>
+            )}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+            {guess !== word && attempts >= 3 && (
+              <Text textAlign="center" fontSize="xl" color="red.500">
+                Better luck next time!
+              </Text>
+            )}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            {attempts < 3 && (
+              <Text textAlign="center" fontSize="sm" color="gray.400">
+                {attempts} of 3 attempts
+              </Text>
+            )}
+
+            {product && (attempts >= 3 || guess === word) && (
+              <Card as="a" href={product.url} target="_blank">
+                <CardHeader>{product.name}</CardHeader>
+                <CardBody>
+                  <Image
+                    src={product.image}
+                    alt={product.alt}
+                    w="48"
+                    h="48"
+                    objectFit="cover"
+                  />
+                </CardBody>
+                <CardFooter>
+                  <Text fontSize="sm" color="gray.400">
+                    {product.desc}
+                  </Text>
+                </CardFooter>
+              </Card>
+            )}
+
+            <Button size="sm" variant="outline" onClick={getWords}>
+              Reload
+            </Button>
+          </VStack>
+        </Container>
+      </Box>
     </main>
-  )
+  );
 }
