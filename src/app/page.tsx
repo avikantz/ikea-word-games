4066cce8009fef6f929f92919e27842040034560
avikantz <avikantsainidbz@gmail.com>
2023-08-19 13:30:43 +0530

    Auto redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
 "use client";
 
+import { useEffect } from "react";
 import { Center, Heading } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+
+import { JUMBLE } from "@/utils/paths";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push(JUMBLE);
+  }, [router]);
+
   return (
     <main>
       <Center h="calc(100vh - 72px)">
