a5137cc5a64e6f495b411314ef3f1a3e9108659e
avikantz <avikantsainidbz@gmail.com>
2023-08-23 20:28:51 +0530

    Adds guess option to bildval game

--- src/app/bildval/[mode]/page.tsx
+++ src/app/bildval/[mode]/page.tsx
@@ -1,7 +1,19 @@
 "use client";
 
 import { useCallback, useEffect, useRef, useState } from "react";
-import { Button, HStack, Heading, Spacer, Tag, Text, VStack, useDisclosure } from "@chakra-ui/react";
+import {
+  Button,
+  Container,
+  HStack,
+  Heading,
+  Spacer,
+  Tag,
+  Text,
+  VStack,
+  useDisclosure,
+  Spinner,
+  SimpleGrid,
+} from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
 import { useAnimate } from "framer-motion";
 
@@ -11,6 +23,8 @@ import { PATH_BILDVAL } from "@/utils/paths";
 import { BILDVAL } from "@/utils/constants";
 import { useScores } from "@/hooks/useScores";
 import { useBildval } from "@/hooks/useBildval";
+import { WordDisplay } from "@/components/wordDisplay";
+import { BildvalGuessOption } from "@/components/bildval";
 
 function BildvalGameMode({ params }: { params: { mode: string } }) {
   const router = useRouter();
@@ -37,7 +51,7 @@ function BildvalGameMode({ params }: { params: { mode: string } }) {
   const [score, setScore] = useState<number>(0);
   const [multiplier, setMultiplier] = useState<number>(1);
 
-  const [success, setSuccess] = useState<boolean>(false);
+  const [showSolution, setShowSolution] = useState<boolean>(false);
 
   // Scoring
   const { scores, saveScore } = useScores({ game: "bildval", mode: difficulty });
@@ -70,7 +84,7 @@ function BildvalGameMode({ params }: { params: { mode: string } }) {
 
     if (bildvalRound) words.push(bildvalRound.solution.name);
 
-    setSuccess(false);
+    setShowSolution(false);
   }, [getBildvalRound, words]);
 
   // Get a new word on load
@@ -127,72 +141,86 @@ function BildvalGameMode({ params }: { params: { mode: string } }) {
     } else {
       // Reset multiplier
       setMultiplier(1);
+      // Highlight correct option if failure
+      setShowSolution(true);
     }
 
-    onNextRound();
+    // Delay next round
+    setTimeout(() => {
+      onNextRound();
+    }, 1500);
   };
 
   return (
-    <VStack alignItems={{ base: "stretch", md: "center" }} spacing={{ base: 4, md: 8 }}>
-      <HStack spacing="4">
-        <Heading textAlign="center" textTransform="capitalize">
-          Bildval {difficulty}
-        </Heading>
-      </HStack>
-
-      {round > 0 && (
-        <HStack minW={{ base: "full", md: "500" }} justifyContent="space-between">
-          <Tag size="lg" bg="blue.500" color="white">
-            Round {round} of {BILDVAL.MAX_ROUNDS}
-          </Tag>
-
-          <Spacer />
-
-          <Tag ref={multiplierRef} size="lg" bg="yellow.500" color="black">
-            {multiplier}x
-          </Tag>
-
-          <Tag ref={scoreRef} size="lg" bg="black" color="white">
-            Score {score}
-          </Tag>
+    <Container maxW="container.lg">
+      <VStack alignItems="stretch" spacing={{ base: 4, md: 8 }}>
+        <HStack justifyContent="center" spacing="4">
+          <Heading textAlign="center" textTransform="capitalize">
+            Bildval {difficulty}
+          </Heading>
         </HStack>
-      )}
 
-      {/* Active game */}
-      {bildvalRound && round > 0 && round <= BILDVAL.MAX_ROUNDS && (
-        <VStack alignItems="stretch" spacing={{ base: 4, md: 8 }}>
-          {/* TODO: Product name */}
-          <Text>{bildvalRound.solution.name}</Text>
+        {round > 0 && (
+          <HStack minW={{ base: "full", md: "500" }} justifyContent="space-between">
+            <Tag size="lg" bg="blue.500" color="white">
+              Round {round} of {BILDVAL.MAX_ROUNDS}
+            </Tag>
 
-          {/* TODO: Guess options */}
-          {bildvalRound.guesses.map((guess) => (
-            <Button key={guess.name} onClick={() => onMatch(guess)}>
-              {guess.desc}
+            <Spacer />
+
+            <Tag ref={multiplierRef} size="lg" bg="yellow.500" color="black">
+              {multiplier}x
+            </Tag>
+
+            <Tag ref={scoreRef} size="lg" bg="black" color="white">
+              Score {score}
+            </Tag>
+          </HStack>
+        )}
+
+        {/* Active game */}
+        {(bildvalRound && round > 0 && round <= BILDVAL.MAX_ROUNDS && (
+          <VStack alignItems="stretch" spacing={{ base: 4, md: 8 }}>
+            <VStack>
+              <Text textAlign="center">What is...</Text>
+              <WordDisplay word={bildvalRound.solution.name} />
+            </VStack>
+
+            <SimpleGrid columns={{ base: 2, md: 4 }} gap={{ base: 4, md: 8 }}>
+              {bildvalRound.guesses.map((guess) => (
+                <BildvalGuessOption
+                  key={guess.name}
+                  guess={guess}
+                  solution={bildvalRound.solution}
+                  showSolution={showSolution}
+                  onClick={() => onMatch(guess)}
+                />
+              ))}
+            </SimpleGrid>
+
+            {/* Skip */}
+            <Button
+              size="sm"
+              variant="outline"
+              alignSelf="center"
+              onClick={onPass}
+              isLoading={!bildvalRound}
+              isDisabled={round > BILDVAL.MAX_ROUNDS || passCount >= BILDVAL.MAX_PASSES}
+            >
+              Pass ({BILDVAL.MAX_PASSES - passCount})
             </Button>
-          ))}
+          </VStack>
+        )) || <Spinner size="lg" />}
 
-          {/* Skip */}
-          <Button
-            size="sm"
-            variant="outline"
-            alignSelf="center"
-            onClick={onPass}
-            isLoading={!bildvalRound}
-            isDisabled={round > BILDVAL.MAX_ROUNDS || passCount >= BILDVAL.MAX_PASSES || success}
-          >
-            Pass ({BILDVAL.MAX_PASSES - passCount})
-          </Button>
-        </VStack>
-      )}
-
-      <GameOverModal
-        isOpen={isOpenGameOverModal}
-        onClose={onCloseGameOverModal}
-        score={score}
-        scores={scores}
-        onCloseComplete={() => router.replace(PATH_BILDVAL)}
-      />
-    </VStack>
+        <GameOverModal
+          isOpen={isOpenGameOverModal}
+          onClose={onCloseGameOverModal}
+          score={score}
+          scores={scores}
+          onCloseComplete={() => router.replace(PATH_BILDVAL)}
+        />
+      </VStack>
+    </Container>
   );
 }
 
