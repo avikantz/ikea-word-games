f11b13498f83af6fcc6f255fbebc5084c5cb7559
avikantz <avikantsainidbz@gmail.com>
2023-08-27 12:51:44 +0530

    Setups i18n

---
+++ src/app/[lang]/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import "@fontsource-variable/open-sans";
+import { dir } from "i18next";
+
+import { languages } from "@/app/i18n/settings";
+import { Providers } from "../providers";
+
+export async function generateStaticParams() {
+  return languages.map((lang) => ({ lang }));
+}
+
+export const metadata: Metadata = {
+  title: "IKEA Word Games",
+  description: "Word games with IKEA product names",
+};
+
+interface RootLayoutParams {
+  children: React.ReactNode;
+  params: {
+    lang: string;
+  };
+}
+
+export default function RootLayout({ children, params: { lang } }: RootLayoutParams) {
+  return (
+    <html lang={lang} dir={dir(lang)}>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -2,26 +2,26 @@
 
 import { Box, Container, Heading, SimpleGrid, Text } from "@chakra-ui/react";
 
+import { useTranslation } from "@/app/i18n/client";
 import { PATH_BILDVAL, PATH_JUMBLE } from "@/utils/paths";
 import { ModeCard } from "@/components";
 
-export default function Home() {
+export default function Home({ params: { lang } }: { params: { lang: string } }) {
+  const { t } = useTranslation(lang);
+  const { t: j } = useTranslation(lang, "jumble");
+
   return (
     <main>
       <Box h="100vh">
         <Container py={{ base: 4, md: 12 }} h="full" maxW="container.xl">
           <Heading textAlign="center" mb={{ base: 4, md: 8 }}>
-            IKEA Word Games
+            {t("title")}
           </Heading>
 
-          <Text textAlign="center">A fun collection of word games based on IKEA products.</Text>
+          <Text textAlign="center">{t("desc")}</Text>
 
           <SimpleGrid columns={{ base: 1, md: 3 }} spacing={{ base: 4, lg: 8 }} py={{ base: 4, md: 8 }}>
-            <ModeCard
-              title="Jumble"
-              desc="Form a word from the given letters to match an IKEA product."
-              href={PATH_JUMBLE}
-            />
+            <ModeCard title={j("title")} desc={j("desc")} href={PATH_JUMBLE} />
             <ModeCard title="Bildval" desc="Guess what picture matches the given product name." href={PATH_BILDVAL} />
             <ModeCard title="Scrabble" isDisabled desc="Coming soon!" />
           </SimpleGrid>
