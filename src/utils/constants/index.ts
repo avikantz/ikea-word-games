667d50ae772f2aa0e889de1aabcc54ec2395801b
avikantz <avikantsainidbz@gmail.com>
2023-09-02 18:01:54 +0530

    Fix base url once again

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -3,6 +3,9 @@ export * as BILDVAL from "./bildval";
 export * as ORDVAL from "./ordval";
 export * as VADARJAG from "./vadarjag";
 
-export const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/`
-  : `http://localhost:${process.env.PORT ?? 3000}/`;
+export const BASE_URL =
+  typeof window !== undefined
+    ? window.location.origin
+    : process.env.NEXT_PUBLIC_BASE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/`
+    : `http://localhost:${process.env.PORT ?? 3000}/`;
