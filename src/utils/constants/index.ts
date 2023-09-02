12ecef979e476369563eba9459ae0b608a1305da
avikantz <avikantsainidbz@gmail.com>
2023-09-02 18:04:23 +0530

    Stupid undefined bug

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -4,7 +4,7 @@ export * as ORDVAL from "./ordval";
 export * as VADARJAG from "./vadarjag";
 
 export const BASE_URL =
-  typeof window !== undefined
+  typeof window !== "undefined"
     ? window.location.origin
     : process.env.NEXT_PUBLIC_BASE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL
     ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/`
