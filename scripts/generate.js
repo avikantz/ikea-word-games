39a711de17c01442b0b7afdc7c4a42a95e68366b
avikantz <avikantsainidbz@gmail.com>
2023-09-22 16:12:32 +0530

    Update scripts

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,6 +1,9 @@
-const { select } = require("@inquirer/prompts");
-// const https = require("https");
+/**
+ * Generates maps and list of product names we need for the game.
+ */
+
 const fs = require("fs");
+const { select } = require("@inquirer/prompts");
 
 // ---------------------------------------------------------------------------------------------------------------------
 
@@ -9,7 +12,7 @@ const LOCALES = {
   en: "gb/en",
   "en-US": "us/en",
   "en-CA": "ca/en",
-  "en-IN": "en/in",
+  "en-IN": "in/en",
   fr: "fr/fr",
   es: "es/es",
   "es-MX": "mx/es",
@@ -179,10 +182,17 @@ function getUniqueCombinations(list = []) {
 // ---------------------------------------------------------------------------------------------------------------------
 
 function generateFromMasterList(masterItems, locale) {
-  fs.mkdirSync(locale);
-  fs.mkdirSync(`${locale}/names`);
-  fs.mkdirSync(`${locale}/unique`);
-  fs.mkdirSync(`${locale}/items`);
+  console.log("\n----------------------------------------\n");
+  console.log(`>> Processing ${masterItems.length} items for "${locale}".\n`);
+
+  console.log(`\t./${locale}/names - List of all non-duplicate product names`);
+  console.log(`\t./${locale}/unique - List of all non-duplicate and non-combinatorial product names`);
+  console.log(`\t./${locale}/items - List and name-item map of all products\n`);
+
+  // Create directories
+  fs.mkdirSync(`${locale}/names`, { recursive: true });
+  fs.mkdirSync(`${locale}/unique`, { recursive: true });
+  fs.mkdirSync(`${locale}/items`, { recursive: true });
 
   const filteredList = [];
   const names = [];
@@ -193,7 +203,7 @@ function generateFromMasterList(masterItems, locale) {
     try {
       const { name, typeName, mainImageUrl, mainImageAlt, pipUrl, id, contextualImageUrl } =
         item.type === "FEATURED_PRODUCT" ? item.featuredProduct : item.product;
-  
+
       // Only need all of these info
       const filteredItem = {
         name,
@@ -204,17 +214,17 @@ function generateFromMasterList(masterItems, locale) {
         url: pipUrl,
         id,
       };
-  
+
       // Filter out non unique, short, and multi word names
       if (!names.includes(name) && !name.includes(" ") && name.length >= 4 && name.length <= 10) {
         names.push(name);
-  
+
         itemsMap[name] = filteredItem;
-  
+
         filteredList.push(filteredItem);
       }
     } catch (error) {
-      console.error('>> Unable to parse item', locale, item);
+      console.error("\t\t!! Unable to parse item", locale, item);
     }
   }
 
@@ -225,37 +235,46 @@ function generateFromMasterList(masterItems, locale) {
     const lNames = names.filter((name) => name.length === length);
 
     // Write list of all names of length
-    fs.writeFileSync(`./${locale}/names/${length}.json`, JSON.stringify(lNames, null, 2));
+    fs.writeFileSync(`./${locale}/names/${length}.json`, JSON.stringify(lNames));
+    console.log(`\t./${locale}/names/${length}.json: ${lNames.length} items`);
 
     const singleNames = getUniqueCombinations(lNames);
 
     // Write list of all single names of length
-    fs.writeFileSync(`./${locale}/unique/${length}.json`, JSON.stringify(singleNames, null, 2));
+    fs.writeFileSync(`./${locale}/unique/${length}.json`, JSON.stringify(singleNames));
+    console.log(`\t./${locale}/unique/${length}.json: ${singleNames.length} items`);
   });
 
   // List of top 100 names
-  fs.writeFileSync(`./${locale}/unique/easy.json`, JSON.stringify(uniqueNames.slice(0, 100).sort(), null, 2));
+  fs.writeFileSync(`./${locale}/unique/easy.json`, JSON.stringify(uniqueNames.slice(0, 100).sort()));
+  console.log(`\t./${locale}/unique/easy.json: ${uniqueNames.slice(0, 100).length} items`);
   // List of top 250 names
-  fs.writeFileSync(`./${locale}/unique/medium.json`, JSON.stringify(uniqueNames.slice(0, 250).sort(), null, 2));
+  fs.writeFileSync(`./${locale}/unique/medium.json`, JSON.stringify(uniqueNames.slice(0, 250).sort()));
+  console.log(`\t./${locale}/unique/medium.json: ${uniqueNames.slice(0, 250).length} items`);
   // List of top 500 names
-  fs.writeFileSync(`./${locale}/unique/hard.json`, JSON.stringify(uniqueNames.slice(0, 500).sort(), null, 2));
+  fs.writeFileSync(`./${locale}/unique/hard.json`, JSON.stringify(uniqueNames.slice(0, 500).sort()));
+  console.log(`\t./${locale}/unique/hard.json: ${uniqueNames.slice(0, 500).length} items`);
   // List of all names
-  fs.writeFileSync(`./${locale}/unique/all.json`, JSON.stringify(uniqueNames.sort(), null, 2));
+  fs.writeFileSync(`./${locale}/unique/all.json`, JSON.stringify(uniqueNames.sort()));
+  console.log(`\t./${locale}/unique/all.json: ${uniqueNames.length} items`);
 
   // List of all names
   names.sort((a, b) => a.localeCompare(b));
-  fs.writeFileSync(`./${locale}/names/all.json`, JSON.stringify(names, null, 2));
+  fs.writeFileSync(`./${locale}/names/all.json`, JSON.stringify(names));
+  console.log(`\t./${locale}/names/all.json: ${names.length} items`);
 
   // name-to-item map and item master list
   filteredList.sort((a, b) => a.name.localeCompare(b.name));
   fs.writeFileSync(`./${locale}/items/map.json`, JSON.stringify(itemsMap));
+  console.log(`\t./${locale}/items/map.json: ${Object.keys(itemsMap).length} items`);
   fs.writeFileSync(`./${locale}/items/list.json`, JSON.stringify(filteredList));
+  console.log(`\t./${locale}/items/list.json: ${filteredList.length} items`);
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
 
 async function generate(locale, sortOrder, count) {
-  // curl https://sik.search.blue.cdtapps.com/fi/fi/search-result-page\?c\=sr\&subcategories-style\=tree-navigation\&sort\=MOST_POPULAR\&size\=10
+  // curl https://sik.search.blue.cdtapps.com/us/en/search-result-page\?c\=sr\&subcategories-style\=tree-navigation\&sort\=MOST_POPULAR\&size\=10
 
   try {
     const ikeaMasterList = await fetch(
@@ -268,11 +287,7 @@ async function generate(locale, sortOrder, count) {
     // Generate the things we need
     generateFromMasterList(masterItems, locale);
 
-    console.log(`>> Generated ${count} products for "${locale}" with sort order ${sortOrder}.`);
-    console.log("Find your files here: ");
-    console.log(`${locale}/names - List of all non-duplicate product names`);
-    console.log(`${locale}/unique - List of all non-duplicate and non-combinatorial product names`);
-    console.log(`${locale}/items - List and name-item map of all products`);
+    console.log("\n----------------------------------------\n");
   } catch (error) {
     console.error("! Failed to generate", error);
   }
@@ -293,12 +308,13 @@ async function main() {
 
   // const count = 10000;
   const count = await select({
-    message: "Number of products to generate",
+    message: "Number of products to search for (max ~28,000)",
     choices: COUNT.map((key) => ({ name: key, value: key })),
   });
 
   if (locale === "all") {
     Object.keys(LOCALES).forEach(async (locale) => {
+      if (locale === "all") return;
       await generate(locale, sortOrder, count);
     });
   } else {
