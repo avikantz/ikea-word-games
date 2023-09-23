480b5daf1450ee4ca146aa95e5100278548fce5b
avikantz <avikantsainidbz@gmail.com>
2023-09-23 15:44:22 +0530

    Metadata on about and faq pages

---
+++ src/app/[lang]/faq/layout.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next";
+
+import { getTFunction } from "@/app/i18n";
+import { PageProps } from "@/interfaces/page";
+
+export async function generateMetadata({ params: { lang } }: PageProps): Promise<Metadata> {
+  const t = await getTFunction(lang);
+
+  const title = t("faq");
+
+  return {
+    title,
+  };
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
