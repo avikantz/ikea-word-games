930c4774f8fc265c92050e14a9e56a72142c7b25
avikantz <avikantsainidbz@gmail.com>
2023-09-24 10:55:58 +0530

    Disable scroll snapping

--- src/components/gameContainer.tsx
+++ src/components/gameContainer.tsx
@@ -9,25 +9,18 @@ interface GameContainerProps extends Partial<BoxProps> {
   shouldSnap?: boolean;
 }
 
-export const GameContainer = forwardRef<HTMLDivElement, GameContainerProps>(
-  ({ shouldSnap, children, ...props }, ref) => (
-    <Box
-      minH="100vh"
-      id="gameContainer"
-      scrollSnapAlign={shouldSnap ? { md: "center" } : undefined}
-      {...props}
-      ref={ref}
-    >
-      <VStack
-        alignItems="stretch"
-        py={PADDING.DEFAULT}
-        minH="100vh"
-        justifyContent="space-between"
-        spacing={PADDING.LG}
-      >
-        {children}
-      </VStack>
-    </Box>
-  ),
-);
+export const GameContainer = forwardRef<HTMLDivElement, GameContainerProps>(({ shouldSnap, children, ...props }, ref) => (
+  <Box
+    minH="100vh"
+    id="gameContainer"
+    // TODO: Enable this when we have a better way to handle the scroll snap
+    // scrollSnapAlign={shouldSnap ? { md: "center" } : undefined}
+    {...props}
+    ref={ref}
+  >
+    <VStack alignItems="stretch" py={PADDING.DEFAULT} minH="100vh" justifyContent="space-between" spacing={PADDING.LG}>
+      {children}
+    </VStack>
+  </Box>
+));
 GameContainer.displayName = "GameContainer";
